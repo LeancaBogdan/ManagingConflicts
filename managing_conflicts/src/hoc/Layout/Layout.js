d3cd74a378e9668da86727866960b87a67ccdbba
Alex Popsor <popsor.balex@yahoo.com>
2020-05-13 01:14:37 +0300

    rename Aux component to Auxiliary

--- managing_conflicts/src/hoc/Layout/Layout.js
+++ managing_conflicts/src/hoc/Layout/Layout.js
@@ -1,17 +1,17 @@
 import React from 'react';
 
 import classes from './Layout.module.css';
-import Aux from '../Aux/Aux';
+import Auxiliary from '../Auxiliary/Auxiliary';
 import Toolbar from '../../components/Toolbar/Toolbar';
 
 const layout = (props) => {
   return (
-    <Aux>
+    <Auxiliary>
       <Toolbar />
       <main className={classes.Content}>
         {props.children}
       </main>
-    </Aux>
+    </Auxiliary>
   );
 }
 
