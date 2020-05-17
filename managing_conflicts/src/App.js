e2826d9b2d1511eab197f8a53c4e933c160f4b7c
Alex Popsor <popsor.balex@yahoo.com>
2020-05-17 21:15:42 +0300

    wip scenario create/edit screen

--- managing_conflicts/src/App.js
+++ managing_conflicts/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 import Layout from './hoc/Layout/Layout';
 import MainBody from './containers/MainBody/MainBody';
+import Scenario from './containers/Scenario/Scenario';
 
 function App() {
   return (
     <Layout>
-      <MainBody />
+      {/* <MainBody /> */}
+      <Scenario />
     </Layout>
   );
 }
---
+++ managing_conflicts/src/containers/Scenario/Scenario.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+import Auxiliary from '../../hoc/Auxiliary/Auxiliary';
+
+class Scenario extends Component {
+  state = {
+    question: null,
+    currentQuestion: null,
+    create: true,
+    showModal: false
+  }
+
+  render() {
+    return (
+      <Auxiliary>
+        
+      </Auxiliary>
+    );
+  }
+}
+
+export default Scenario;
