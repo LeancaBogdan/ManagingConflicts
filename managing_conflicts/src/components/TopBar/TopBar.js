2882d6bb3f3252985fbe147d0b6f40eb0ca48bd7
Madalina Boca <madalinaboca37@gmail.com>
2020-05-20 23:03:59 +0300

    changed logo, centered toggle

--- managing_conflicts/src/components/TopBar/TopBar.js
+++ managing_conflicts/src/components/TopBar/TopBar.js
@@ -3,16 +3,26 @@ import React from 'react';
 import classes from './TopBar.module.css';
 import ToggleBar from './ToggleBar/ToggleBar';
 import Search from '../TopBar/Search/Search';
+import Grid from '@material-ui/core/Grid';
 
 const topBar = (props) => {
-  return (
-    <div className={classes.TopBar}>
-      <ToggleBar className={classes.Toggle}
-        showBrochures={props.showBrochures}
-        toggleClicked={props.toggleClicked}/>
-      <Search search={props.search}/>
-    </div>
-  );
+    return (
+        <div className={classes.TopBar}>
+            <Grid container justify="space-evenly" alignItems="baseline">
+                <Grid item xs={4}/>
+                <Grid item xs={4} >
+                    <ToggleBar className={classes.Toggle}
+                               showBrochures={props.showBrochures}
+                               toggleClicked={props.toggleClicked}/>
+                </Grid>
+                <Grid item xs={1}>
+                </Grid>
+                <Grid item xs={3}  >
+                    <Search search={props.search}/>
+                </Grid>
+            </Grid>
+        </div>
+    );
 }
 
 export default topBar;
