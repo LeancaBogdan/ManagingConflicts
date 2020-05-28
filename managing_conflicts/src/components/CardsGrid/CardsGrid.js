f744768d94cd767bd1bfb7395d57dba1f6fcc713
Adrian-Bogdan Leancă <leancabogdan@outlook.com>
2020-05-29 00:02:58 +0300

    * Updated 2 packages to solve vulnerabilities * Properly indented CardsGrid.js

--- managing_conflicts/src/components/CardsGrid/CardsGrid.js
+++ managing_conflicts/src/components/CardsGrid/CardsGrid.js
@@ -5,39 +5,39 @@ import Card from './Card/Card';
 import EmptyCard from './EmptyCard/EmptyCard'
 
 const cardsGrid = (props) => {
-  let cards = []
+    let cards;
 
-  if (props.showBrochures) {
-    cards = props.brochures.map(brochure => {
-      const scenarios =[]
-      props.scenarios.forEach(scenario => {
-        if (scenario.brochure_id === brochure.id){
-          scenarios.push({id: scenario.id, name: scenario.name})
-        }
-      })
-      return <Card 
-        key={brochure.id}
-        type="brochure"
-        name={brochure.name}
-        body={scenarios}/>
-    })
-  } else {
-    cards = props.scenarios.map(scenario => {
-      return <Card 
-        key={scenario.id}
-        type="scenario"
-        name={scenario.name}
-        body={scenario.description}/>
-    })
-  }
+    if (props.showBrochures) {
+        cards = props.brochures.map(brochure => {
+            const scenarios = []
+            props.scenarios.forEach(scenario => {
+                if (scenario.brochure_id === brochure.id) {
+                    scenarios.push({id: scenario.id, name: scenario.name})
+                }
+            })
+            return <Card
+                key={brochure.id}
+                type="brochure"
+                name={brochure.name}
+                body={scenarios}/>
+        })
+    } else {
+        cards = props.scenarios.map(scenario => {
+            return <Card
+                key={scenario.id}
+                type="scenario"
+                name={scenario.name}
+                body={scenario.description}/>
+        })
+    }
 
-  return (
-    <div className={classes.CardsGrid}>
-      <EmptyCard 
-        clicked={props.emptyClicked}/>
-      {cards}
-    </div>
-  );
+    return (
+        <div className={classes.CardsGrid}>
+            <EmptyCard
+                clicked={props.emptyClicked}/>
+            {cards}
+        </div>
+    );
 }
 
 export default cardsGrid;
