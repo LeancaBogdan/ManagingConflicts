8e28566b5bede7b2c2480adb832d84eea28aabfd
Alex Popsor <popsor.balex@yahoo.com>
2020-06-26 23:10:23 +0300

    server

--- managing_conflicts/src/containers/EmailSender/EmailSender.js
+++ managing_conflicts/src/containers/EmailSender/EmailSender.js
@@ -2,12 +2,89 @@ import React, { Component } from 'react'
 
 import classes from './EmailSender.module.css'
 import Auxiliary from '../../hoc/Auxiliary/Auxiliary'
+import Button from '../../components/UI/Button/Button'
+import axios from 'axios'
 
 class EmailSender extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      brochureId: props.id,
+      emailAddresses: "",
+      subject: "",
+      message: "",
+    }
+  }
+
+  cancel = () => {
+    this.props.history.goBack()
+  }
+
+  sendEmail = () => {
+    const emailAddresses = this.state.emailAddresses
+    const subject = this.state.subject
+    const message = this.state.message
+
+    emailAddresses.split(',').map(email => {
+      const data = {
+        email: email.trim(),
+        subject: subject,
+        message: message
+      }
+
+      axios.post('/api/sendMail', data)
+    })
+  }
+
+  handleAddressesChanged = (event) => {
+    const addresses = event.target.value
+    this.setState({emailAddresses: addresses})
+  }
+
+  handleSubjectChanged = (event) => {
+    const subject = event.target.value
+    this.setState({subject: subject})
+  }
+
+  handleMessageSend = (event) => {
+    const message = event.target.value
+    this.setState({message: message})
+  }
+
   render() {
     return(
       <Auxiliary>
-        <h3>Hello to email sender</h3>
+        <div className={classes.ButtonsContainer}>
+          <div className={classes.Buttons}>
+            <Button btnType="Danger" clicked={this.cancel}>
+              Anulare
+            </Button>
+            <Button btnType="Success" clicked={this.sendEmail}>
+              Trimite
+            </Button>
+          </div>
+        </div>
+        <div className={classes.Instructions}>
+          <ul>
+            <li>Introduceți, separate prin virgulă, toate adresele de email către care doriți sa trimiteți această broșură.</li>
+            <li>În câmpul de mesaj puteți scrie introduce un text care va fi trimis tuturor participantilor. La acest text link-ul catre broșură va fi atasat automat.</li>
+          </ul>
+        </div>
+        <div className={classes.Address}>
+          <label>Trimite catre:</label>
+          <textarea 
+            value={this.state.name} onChange={this.handleAddressesChanged}/>
+        </div>
+        <div className={classes.Subject}>
+          <label>Subiect:</label>
+          <input 
+            value={this.state.name} onChange={this.handleSubjectChanged}/>
+        </div>
+        <div className={classes.Message}>
+          <label>Mesaj</label>
+          <textarea 
+            value={this.state.description} onChange={this.handleMessageSend}/>
+        </div>
       </Auxiliary>
     )
   }
